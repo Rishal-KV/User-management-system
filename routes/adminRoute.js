a4492839972339940d141c73452504cc6f83b3dc
Rishal-KV <rishkv923@gmail.com>
2023-09-05 18:33:23 +0530

    final commit

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const admin_rout = express();
+const admin_route = express();
 const adminController = require("../controllers/adminController");
 const adminAuth = require('../middleware/adminAuth')
-admin_rout.set('view engine', 'ejs');
-admin_rout.set('views', './views');
-admin_rout.use(express.json());
-admin_rout.use(express.urlencoded({ extended: true }));
+admin_route.set('view engine', 'ejs');
+admin_route.set('views', './views');
+admin_route.use(express.json());
+admin_route.use(express.urlencoded({ extended: true }));
 
-admin_rout.get('/',adminAuth.isLoggedOut,adminController.adminLogin)
-admin_rout.post('/dashboard',adminController.verifyAdmin);
+admin_route.get('/',adminAuth.isLoggedOut,adminController.adminLogin)
+admin_route.post('/dashboard',adminController.verifyAdmin);
 
-admin_rout.get('/dashboard', adminAuth.isLoggedIn, adminController.showDashboard);
+admin_route.get('/dashboard', adminAuth.isLoggedIn, adminController.showDashboard);
 
-admin_rout.post('/logout',adminController.adminLogout)
-admin_rout.get('/new-user',adminAuth.isLoggedIn,adminController.newUserLoad) 
-admin_rout.post('/new-user',adminController.addUser)
-admin_rout.get('/edit-user',adminAuth.isLoggedIn,adminController.editUser);
-admin_rout.post('/update-user',adminController.updateUser);
-admin_rout.get('/delete-user',adminController.deleteUser)
-admin_rout.post('/search',adminController.searchUser);
+admin_route.post('/logout',adminController.adminLogout)
+admin_route.get('/new-user',adminAuth.isLoggedIn,adminController.newUserLoad) 
+admin_route.post('/new-user',adminController.addUser)
+admin_route.get('/edit-user',adminAuth.isLoggedIn,adminController.editUser);
+admin_route.post('/update-user',adminController.updateUser);
+admin_route.get('/delete-user',adminController.deleteUser)
+admin_route.post('/search',adminController.searchUser);
 
 
-module.exports = admin_rout
+module.exports = admin_route
 
 
